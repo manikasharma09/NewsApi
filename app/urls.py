03b9af4fece20f2a74ed31049250cb3c1935209b
Abhinav Sharma <abhinavsharma629@gmail.com>
2019-10-08 23:24:22 +0530

    Api Implementation. Frontend Improved

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns=[
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('news', views.news, name="news"),
     ]
